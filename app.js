fc05bf3a0f6c7463e31548e6ca4f1fb0da4c5a94
Andrew <vurtua7@hotmail.com>
2019-02-21 15:09:14 -0500

    Thursday Class

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var io = require('socket.io')(process.env.PORT||5000);
+var shortid = require('shortid');
 
+var players = [];
 var playerCount = 0;
 
 console.log("Server Running");
 
 io.on('connection',function(socket){
     console.log("Connected to Unity");
+    var thisPlayerId = shortid.generate();
 
-    socket.broadcast.emit('spawn');
-    playerCount++;
+    var player = {
+        id:thisPlayerId,
+        position:{
+            v:0,
+            
+        }
+    }
 
-    for(var i = 0; i<playerCount; i++){
+    players[thisPlayerId] = player;
+
+    socket.broadcast.emit('spawn', {id:thisPlayerId});
+    console.log('Sending spawn to new with ID', thisPlayerId);
+
+    for(var i = 0; i<players.length; i++){
         socket.emit('spawn');
-        console.log('Sending spayn to new ');
+        
     }
 
     socket.on('sayhello', function(data){
@@ -24,4 +37,10 @@ io.on('connection',function(socket){
         console.log("Player Disconnected");
         playerCount--;
     } );
+
+    socket.on('move', function(data){
+        data.id = thisPlayerId;
+        console.log("Player Moved", JSON.stringify(data));
+        socket.broadcast.emit('move', data);
+    });
 })
