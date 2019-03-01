d5e768aeb6af8b665b85af5d96b52a162a9eb6a4
Andrew <vurtua7@hotmail.com>
2019-03-01 15:02:17 -0500

    push for friday

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var io = require('socket.io')(process.env.PORT||5000);
 var shortid = require('shortid');
 
 var players = [];
-var playerCount = 0;
+
 
 console.log("Server Running");
 
@@ -19,13 +19,15 @@ io.on('connection',function(socket){
     }
 
     players[thisPlayerId] = player;
-
+    socket.emit('register', {id:thisPlayerId});
     socket.broadcast.emit('spawn', {id:thisPlayerId});
-    console.log('Sending spawn to new with ID', thisPlayerId);
+    
 
-    for(var i = 0; i<players.length; i++){
-        socket.emit('spawn');
-        
+    for(var playerId in players){
+        if(playerId == thisPlayerId)
+        continue;
+        socket.emit('spawn', players[playerId]);
+        console.log('Sending spawn to new with ID', thisPlayerId);
     }
 
     socket.on('sayhello', function(data){
@@ -35,12 +37,21 @@ io.on('connection',function(socket){
 
     socket.on('disconnect', function(){
         console.log("Player Disconnected");
-        playerCount--;
+        delete players[thisPlayerId];
+        socket.broadcast.emit('disconnected', {id:thisPlayerId});
     } );
 
     socket.on('move', function(data){
         data.id = thisPlayerId;
-        console.log("Player Moved", JSON.stringify(data));
+       // console.log("Player Moved", JSON.stringify(data));
         socket.broadcast.emit('move', data);
     });
+
+    socket.on('updatePosition', function(data){
+        data.id = thisPlayerId;
+        socket.broadcast.emit('updatePosition', data);
+    });
+
+    
+
 })
