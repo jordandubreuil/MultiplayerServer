d5aff058c19bac255b5fee3133e61e59fde12c93
Andrew <vurtua7@hotmail.com>
2019-03-07 14:11:59 -0500

    Final Update

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var io = require('socket.io')(process.env.PORT||5000);
 var shortid = require('shortid');
+var express = require('express');
+var app = express();
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var db = require('./config/database');
+
+//gets rid of warning for Mongoose
+mongoose.Promise = global.Promise;
+
+//connect to mongodb using mongoose
+mongoose.connect(db.mongoURI,{
+    useMongoClient:true
+})
+.then(function(){console.log("MongoDB Connected")})
+.catch(function(err){console.log(err)});
+
+//Load in Models
+require('./models/Users');
+var Users = mongoose.model('Users');
 
 var players = [];
 
@@ -8,6 +28,7 @@ console.log("Server Running");
 
 io.on('connection',function(socket){
     console.log("Connected to Unity");
+    socket.emit('connected');
     var thisPlayerId = shortid.generate();
 
     var player = {
@@ -22,13 +43,33 @@ io.on('connection',function(socket){
     socket.emit('register', {id:thisPlayerId});
     socket.broadcast.emit('spawn', {id:thisPlayerId});
     socket.broadcast.emit('requestPosition');
-
+    
     for(var playerId in players){
         if(playerId == thisPlayerId)
         continue;
         socket.emit('spawn', players[playerId]);
         console.log('Sending spawn to new with ID', thisPlayerId);
     }
+
+    socket.on('senddata', function(data){
+        console.log(JSON.stringify(data));
+        var newUser = {
+            name:data.name,
+    
+        }
+        new Users(newUser)
+        .save()
+        .then(function(users){
+            console.log("Sending Data to Database");
+            Users.find({})
+            .then(function(users){
+                console.log(users);
+                socket.emit('hideform', {users});
+            });
+
+            
+        });
+    });
 
     socket.on('sayhello', function(data){
         console.log("Unity Game says hello");
